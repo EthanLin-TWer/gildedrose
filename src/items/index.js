e6c100e97b601acd63ae775defec90f967d7499f
Linesh <linesh.simpcity@gmail.com>
2019-10-22 23:41:16 +0800

    turn my understanding into the code and adding structure

--- src/items/index.js
+++ src/items/index.js
@@ -10,6 +10,14 @@ export class Item {
   }
 
   update() {
+    this.updateQuality()
+    this.updateSellIn()
+    if (this.isExpired()) {
+      this.updateQualityAfterExpiration()
+    }
+  }
+
+  updateQuality() {
     if (!this.isAgedBrie() && !this.isBackstagePass()) {
       if (this.quality > 0) {
         if (!this.isSulfuras()) {
@@ -33,25 +41,33 @@ export class Item {
         }
       }
     }
-    if (!this.isSulfuras()) {
-      this.sellIn = this.sellIn - 1
-    }
-    if (this.sellIn < 0) {
-      if (!this.isAgedBrie()) {
-        if (!this.isBackstagePass()) {
-          if (this.quality > 0) {
-            if (!this.isSulfuras()) {
-              this.quality = this.quality - 1
-            }
+  }
+
+  updateQualityAfterExpiration() {
+    if (!this.isAgedBrie()) {
+      if (!this.isBackstagePass()) {
+        if (this.quality > 0) {
+          if (!this.isSulfuras()) {
+            this.quality = this.quality - 1
           }
-        } else {
-          this.quality = 0
         }
       } else {
-        if (this.quality < 50) {
-          this.quality = this.quality + 1
-        }
+        this.quality = 0
       }
+    } else {
+      if (this.quality < 50) {
+        this.quality = this.quality + 1
+      }
+    }
+  }
+
+  isExpired() {
+    return this.sellIn < 0
+  }
+
+  updateSellIn() {
+    if (!this.isSulfuras()) {
+      this.sellIn = this.sellIn - 1
     }
   }
 
