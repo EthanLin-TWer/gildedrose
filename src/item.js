bc2dbecc1411670c61715221fc69d2400a7db8c5
Linesh <linesh.simpcity@gmail.com>
2019-10-22 09:15:48 +0800

    eliminate feature envy: extract updateItem and move to Item

--- src/item.js
+++ src/item.js
@@ -9,6 +9,55 @@ export class Item {
     this.quality = quality
   }
 
+  updateItem() {
+    if (
+      this.name !== 'Aged Brie' &&
+      this.name !== 'Backstage passes to a TAFKAL80ETC concert'
+    ) {
+      if (this.quality > 0) {
+        if (this.name !== 'Sulfuras, Hand of Ragnaros') {
+          this.quality = this.quality - 1
+        }
+      }
+    } else {
+      if (this.quality < 50) {
+        this.quality = this.quality + 1
+        if (this.name === 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.sellIn < 11) {
+            if (this.quality < 50) {
+              this.quality = this.quality + 1
+            }
+          }
+          if (this.sellIn < 6) {
+            if (this.quality < 50) {
+              this.quality = this.quality + 1
+            }
+          }
+        }
+      }
+    }
+    if (this.name !== 'Sulfuras, Hand of Ragnaros') {
+      this.sellIn = this.sellIn - 1
+    }
+    if (this.sellIn < 0) {
+      if (this.name !== 'Aged Brie') {
+        if (this.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.quality > 0) {
+            if (this.name !== 'Sulfuras, Hand of Ragnaros') {
+              this.quality = this.quality - 1
+            }
+          }
+        } else {
+          this.quality = 0
+        }
+      } else {
+        if (this.quality < 50) {
+          this.quality = this.quality + 1
+        }
+      }
+    }
+  }
+
   toString() {
     return `${this.name}, ${this.sellIn}, ${this.quality}`
   }
